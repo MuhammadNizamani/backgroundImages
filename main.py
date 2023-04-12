f44a1a5829cad22396a543f708b696bfa07af8e3
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-04-12 12:29:00 +0500

    EHN: Image BG is remove and BG became white

---
+++ main.py
@@ -0,0 +1,14 @@
+from PIL import Image
+from rembg import remove
+path = 'ishaque.jpg'
+def remove_image_BG(path):
+    input_image = Image.open(path)
+    output_image = remove(input_image)
+    background_color = (255,255,255)
+    background_image = Image.new('RGB', output_image.size, background_color)
+    background_image.paste(output_image, mask=output_image.convert('RGBA'))
+    background_image.show()
+    background_image.save('./ahmed.png', format='PNG')
+
+
+remove_image_BG(path)
