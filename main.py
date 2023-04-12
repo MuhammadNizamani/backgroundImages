6cf4855d375c6997b4b99c891cef81cbe5a306de
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-04-12 12:38:10 +0500

    FIx: Don't need white BG remove that functionality

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ path = 'ishaque.jpg'
 def remove_image_BG(path):
     input_image = Image.open(path)
     output_image = remove(input_image)
-    background_color = (255,255,255)
-    background_image = Image.new('RGB', output_image.size, background_color)
-    background_image.paste(output_image, mask=output_image.convert('RGBA'))
-    background_image.show()
-    background_image.save('./ahmed.png', format='PNG')
+    # background_color = (255,255,255)
+    # background_image = Image.new('RGB', output_image.size, background_color)
+    # background_image.paste(output_image, mask=output_image.convert('RGBA'))
+    output_image.show()
+    output_image.save('./ahmed.png', format='PNG')
 
 
 remove_image_BG(path)
