e41aaafbdaab7009735abfca3e88ded512f85b18
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-04-13 14:58:43 +0500

    FIX: checking image resulations

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ class ImageBG():
     def __init__(self,path:str, savename:str = None):
         self.path = path
         self.savename= savename
-    def remove_image_BG(self):
+    def remove_image_BG(self, sizeofimage: bool = False):
         input_image = Image.open(self.path)
         output_image = remove(input_image)
         # background_color = (255,255,255)
         # background_image = Image.new('RGB', output_image.size, background_color)
         # background_image.paste(output_image, mask=output_image.convert('RGBA'))
         output_image.show()
+        if sizeofimage:
+            output_image = output_image.resize((300,400))
+            output_image.show()  
+
         saveNameNumber = random.randint(0,10000000) # adding random here to genrater new name for saving file
 
         self.savename = f'{saveNameNumber}.png'
-        print(self.savename)
-        output_image.save(f'./{self.savename}', format='PNG')
+        output_image.save(f'./{self.savename}', format='PNG', optimize=True, quality= 200)
 
-path = 'ishaque.jpg'
+path = 'check1.jpg'
 bgremove = ImageBG(path)
-bgremove.remove_image_BG()
+bgremove.remove_image_BG(sizeofimage=True)
+
+
