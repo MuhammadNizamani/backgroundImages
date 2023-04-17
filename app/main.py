72c4428ad1b102a027151d9ed956cb5f9cb75fce
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-04-17 15:40:41 +0500

    FIX: setting routes folder

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from PIL import Image
 from rembg import remove
 from fastapi.responses import FileResponse
 import tempfile
+from .routers import removebg
 
 # from . import utils
 
@@ -18,18 +19,4 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-@app.post("/removes")
-async def removesbg(file: UploadFile = File(...)):
-    file.filename = f"{uuid.uuid4()}.jpg"
-    contents = await file.read()
-    image = Image.open(io.BytesIO(contents)) 
-    output_image = remove(image)
-    with tempfile.NamedTemporaryFile(delete=False) as tmp:
-        output_image.save(tmp, format = "png")
-        tmp.flush()
-        tmp.seek(0)
-        # background_color = (255,255,255)
-        # background_image = Image.new('RGB', output_image.size, background_color)
-        # background_image.paste(output_image, mask=output_image.convert('RGBA'))
-    # output_image.show()
-    return FileResponse(tmp.name, media_type="image/png")
+app.include_router(removebg.router)
